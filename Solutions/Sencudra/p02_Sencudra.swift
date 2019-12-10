f2a614120e8d385dd81bfeb9249dfb97334fc081
Vladislav Tarasevich <vlad.tarasevich27@gmail.com>
2019-12-10 21:06:50 +0300

    Solution for P02

---
+++ Solutions/Sencudra/p02_Sencudra.swift
@@ -0,0 +1,17 @@
+
+import NinetyNineSwiftProblems
+
+extension List {
+
+    /// P02 (*) Find the last but one element of a linked list.
+    /// - returns: T last but one element of the linked list
+    /// - author: Vlad Tarasevich (vlad.tarasevich27@gmail.com)
+    /// - complexity: O(n)
+    public var penultimate: T? {
+        guard let next = nextItem else {
+            return nil
+        }
+        return nextItem?.penultimate ?? value
+    }
+
+}
