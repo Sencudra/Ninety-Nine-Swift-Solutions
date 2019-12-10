e0375278f7ce6f93aade22114aff8667a881b1ff
Vladislav Tarasevich <vlad.tarasevich27@gmail.com>
2019-12-11 01:00:22 +0300

    Solution for P05

---
+++ Solutions/Sencudra/p05_Sencudra.swift
@@ -0,0 +1,23 @@
+
+import NinetyNineSwiftProblems
+
+extension List {
+
+    /// P05 (*) Reverse a linked list.
+    /// - returns: Copy of reversed linked list
+    /// - author: Vladislav Tarasevich (vlad.tarasevich27@gmail.com)
+    /// - complexity: O(n)
+    func reverse() -> List {
+        var newHead = List(value)
+        var oldHead: List = self
+        
+        while oldHead.nextItem != nil {
+            oldHead = oldHead.nextItem!
+            let newElement = List(oldHead.value)
+            newElement.nextItem = newHead
+            newHead = newElement
+        }
+        return newHead
+    }
+
+}
