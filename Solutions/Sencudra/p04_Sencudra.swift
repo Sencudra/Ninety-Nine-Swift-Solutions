f614345a3b0e2148093771f11a2f14bb8d969fb0
Vladislav Tarasevich <vlad.tarasevich27@gmail.com>
2019-12-10 21:33:53 +0300

    Solution for P04

---
+++ Solutions/Sencudra/p04_Sencudra.swift
@@ -0,0 +1,14 @@
+
+import NinetyNineSwiftProblems
+
+extension List {
+
+    /// P 04 (*) Find the number of elements of a linked list.
+    /// - returns: Number of elements of a linked list.
+    /// - author: Vladislav Tarasevich (vlad.tarasevich27@gmail.com)
+    /// - complexity: O(n)
+    var length: Int {
+        return (nextItem?.length ?? 0) + 1
+    }
+    
+}
