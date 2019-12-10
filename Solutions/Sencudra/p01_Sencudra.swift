874576ccbe661d6318ec74fd38adcc9b2299a01e
Vladislav Tarasevich <vlad.tarasevich27@gmail.com>
2019-12-10 20:36:30 +0300

    Solution for P01

---
+++ Solutions/Sencudra/p01_Sencudra.swift
@@ -0,0 +1,14 @@
+
+import NinetyNineSwiftProblems
+
+extension List {
+
+    /// P01 (*) Find the last element of a linked list.
+    /// - returns: T last element of the linked list
+    /// - author: Vlad Tarasevich (vlad.tarasevich27@gmail.com)
+    /// - complexity: O(n)
+    public var last: T {
+        return nextItem?.last ?? value
+    }
+
+}
