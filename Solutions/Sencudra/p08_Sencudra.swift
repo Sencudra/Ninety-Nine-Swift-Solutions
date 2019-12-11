f7e41aa1693befe516f7ead40b973111b238f607
Vladislav Tarasevich <vladislav.tarasevich@kaspersky.com>
2019-12-11 14:20:27 +0300

    Solution for P08

---
+++ Solutions/Sencudra/p08_Sencudra.swift
@@ -0,0 +1,26 @@
+
+import NinetyNineSwiftProblems
+
+extension List where T: Equatable {
+
+    /// P08 (**) Eliminate consecutive duplicates of linked list elements.
+    /// - returns: Returns a copy of a list without duplicates.
+    /// - author: Vladislav Tarasevich (vlad.tarasevich27@gmail.com)
+    /// - complexity: O(n)
+    func compress() -> List {
+        let head = List(value)
+        var tail = head
+        var oldHead = self
+        
+        while let next = oldHead.nextItem {
+            if next.value != oldHead.value {
+                let newElement = List(next.value)
+                tail.nextItem = newElement
+                tail = newElement
+            }
+            oldHead = next
+        }
+        return head
+    }
+    
+}
