6f506511f862dd095f6da8fcc1e22ca414461456
Vladislav Tarasevich <vladislav.tarasevich@kaspersky.com>
2019-12-11 17:53:14 +0300

    Solution for p12

---
+++ Solutions/Sencudra/p12_Sencudra.swift
@@ -0,0 +1,33 @@
+
+import NinetyNineSwiftProblems
+
+extension List {
+
+    /// P12 (**)  Decode a run-length encoded linked list.
+    /// - returns: Returns decoded List
+    /// - author: Vladislav Tarasevich (vlad.tarasevich27@gmail.com)
+    /// - complexity: O(n)
+    func decode() -> List<String> {
+        var oldHead = self
+        guard let (count, char) = oldHead.value as? (Int, String) else { return List<String>("") }
+        let head = List<String>(char)
+        var tail = head
+        
+        for _ in 1...count - 1 {
+            let newElement = List<String>(char)
+            tail.nextItem = newElement
+            tail = newElement
+        }
+
+        while let (count, value) = oldHead.nextItem?.value as? (Int, String) {
+            for _ in 0...count - 1 {
+                let newElement = List<String>(value)
+                tail.nextItem = newElement
+                tail = newElement
+            }
+            oldHead = oldHead.nextItem!
+        }
+        return head
+    }
+
+}
