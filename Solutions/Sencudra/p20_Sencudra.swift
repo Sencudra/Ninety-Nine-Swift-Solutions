4e4374b4ba501c0859dcad8dd7ac3a7a64ff6219
Vladislav Tarasevich <vlad.tarasevich27@gmail.com>
2019-12-16 00:53:15 +0300

    Solution for P20

---
+++ Solutions/Sencudra/p20_Sencudra.swift
@@ -0,0 +1,42 @@
+
+import NinetyNineSwiftProblems
+
+extension List {
+
+    /// P20 (*) Remove the Kth element from a linked list.
+    /// - returns: Returns the tuple with copy of linked list without Kth element and Kth element
+    /// - author: Vladislav Tarasevich (vlad.tarasevich27@gmail.com)
+    /// - complexity: O(n)
+    func remove(at: Int) -> (rest: List?, removed: T?) {
+        var oldHead = self
+        var dropValue: T? = nil
+        var head: List
+        
+        if at == 0 {
+            guard let next = oldHead.nextItem else {
+                return (nil, oldHead.value)
+            }
+            dropValue = oldHead.value
+            oldHead = next
+            head = List(oldHead.value)
+        } else {
+            head = List(oldHead.value)
+        }
+        var tail = head
+        var counter = 0
+        
+        while let next = oldHead.nextItem {
+            counter += 1
+            if at == counter {
+                dropValue = next.value
+            } else {
+                let newElement = List(next.value)
+                tail.nextItem = newElement
+                tail = newElement
+            }
+            oldHead = next
+        }
+        return (head, dropValue)
+    }
+
+}
