3b83c126ddecd2cd2ee62c048a47f11550a17db3
Vladislav Tarasevich <vlad.tarasevich27@gmail.com>
2019-12-16 00:50:39 +0300

    Solution for P15

---
+++ Solutions/Sencudra/p15_Sencudra.swift
@@ -0,0 +1,32 @@
+
+import NinetyNineSwiftProblems
+
+extension List {
+
+    /// P15 (**) Duplicate the elements of a linked list a given number of times.
+    /// - returns: Returns the list with a given number of times duplicated values.
+    /// - author: Vladislav Tarasevich (vlad.tarasevich27@gmail.com)
+    /// - complexity: O(n)
+    func duplicate(times: Int) -> List {
+        var oldHead = self
+        let head = List(oldHead.value)
+        var tail = head
+        
+        for _ in 0..<(times - 1) {
+            let newElement = List(oldHead.value)
+            tail.nextItem = newElement
+            tail = newElement
+        }
+                
+        while let next = oldHead.nextItem {
+            for _ in 0..<times {
+                let newElement = List(next.value)
+                tail.nextItem = newElement
+                tail = newElement
+            }
+            oldHead = next
+        }
+        return head
+    }
+
+}
