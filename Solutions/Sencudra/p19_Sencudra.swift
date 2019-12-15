04b35f87954b3ad4ecb80cbd82e4f6d6e4385d47
Vladislav Tarasevich <vlad.tarasevich27@gmail.com>
2019-12-16 00:52:54 +0300

    Solution for P19

---
+++ Solutions/Sencudra/p19_Sencudra.swift
@@ -0,0 +1,24 @@
+
+import NinetyNineSwiftProblems
+
+extension List {
+
+    /// P19 (**) Rotate a list N places to the left.
+    /// - returns: Returns a rotated list copy N places to the left.
+    /// - author: Vladislav Tarasevich (vlad.tarasevich27@gmail.com)
+    /// - complexity: O(n)
+    func rotate(amount: Int) -> List {
+        let length = self.length
+        let index = amount < 0 ? length - abs(amount) : amount % length
+        let (headOne, headTwo) = self.split(at: index)
+        
+        if let headTwo = headTwo {
+            headTwo.lastElement.nextItem = headOne
+            return headTwo
+        } else {
+            return headOne!
+        }
+        
+    }
+
+}
