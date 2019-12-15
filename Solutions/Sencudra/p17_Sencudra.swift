8347ad8e58f25fe7bae591cdef66676c0c0bbd8b
Vladislav Tarasevich <vlad.tarasevich27@gmail.com>
2019-12-16 00:52:02 +0300

    Solution for P17

---
+++ Solutions/Sencudra/p17_Sencudra.swift
@@ -0,0 +1,36 @@
+
+import NinetyNineSwiftProblems
+
+extension List {
+
+    /// P17 (*) Split a linked list into two parts.
+    /// - returns: Returns a tuple with two parts of a linked list splited at index Nth
+    /// - author: Vladislav Tarasevich (vlad.tarasevich27@gmail.com)
+    /// - complexity: O(n)
+    func split(at index: Int) -> (left: List?, right: List?) {
+        var oldHead = self
+        let firstElement = List(oldHead.value)
+        let headOne = index == 0 ? nil : firstElement
+        var headTwo = index == 0 ? firstElement : nil
+        var tail = headOne != nil ? headOne : headTwo
+        var counter = 0
+        
+        while let next = oldHead.nextItem {
+            counter = counter <= index ? counter + 1 : counter
+            
+            if counter == index {
+                headTwo = List(next.value)
+                tail = headTwo
+                oldHead = next
+                continue
+            }
+
+            let newElement = List(next.value)
+            tail?.nextItem = newElement
+            tail = newElement
+            oldHead = next
+        }
+        return (headOne, headTwo)
+    }
+
+}
